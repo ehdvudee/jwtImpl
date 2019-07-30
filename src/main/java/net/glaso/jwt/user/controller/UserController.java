05c5720ca8a88e45195b449c20853fd49f80796c
ehdvudee <ehdvudee@naver.com>
2019-07-30 13:56:04 +0900

    Initial commit

---
+++ src/main/java/net/glaso/jwt/user/controller/UserController.java
@@ -0,0 +1,15 @@
+package net.glaso.jwt.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("")
+    public void user() {
+        System.out.println( "test" );
+    }
+}
